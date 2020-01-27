6492a4ea28143049e0c0ed5ce04d5ae2bdc8ee10
guosai <guosainpu@gmail.com>
2020-01-27 13:26:19 +0800

    通过static测试

---
+++ projects/08/FunctionCalls/StaticsTest/CodeWriter.rb
@@ -0,0 +1,334 @@
+class CodeWriter
+	def initialize()
+		@sam_file
+		@branchIndex = 0
+		@mergeIndex = 0
+		@callIndex = 0
+		@table = {"constant"=>"constant", "local"=>"LCL", "argument"=>"ARG", "this"=>"THIS", "that"=>"THAT", "temp"=>"temp", "pointer"=>"pointer", "static"=>"static"}
+	end
+
+	def setFile(file)
+		@file_name = File.basename(file.path)
+		@sam_file = file
+	end
+
+	def setSourceFileName(fileName)
+		@source_file_name = fileName
+	end
+
+	# 第七章实现
+
+	def writeArithmetic(command) #输出相应的算数操作代码
+		asm_cmd = ''
+		asm_cmd = arithmetic(command)
+		if asm_cmd.length > 0
+			@sam_file.write(asm_cmd)
+		end	
+	end
+
+	def writePushPop(command, segment, index) #输出相应的push,pop操作代码
+		asm_cmd = ''
+		segment = @table["#{segment}"]
+		if command == 'C_PUSH' && segment =='constant' #实现简单的push const
+			asm_cmd << pushConstant(index)
+		elsif command == 'C_PUSH' #实现push segment存储的内容(local, argument, this, that)
+			asm_cmd << passArgsAddressToA(segment, index) #A=段地址+偏移地址
+			asm_cmd << "D=M"<< "\n" #段的内容寄存到D
+			asm_cmd << pushDtoStack()
+		elsif command == 'C_POP' #实现pop segment存储的内容(local, argument, this, that)
+			asm_cmd << passArgsAddressToA(segment, index) #A=段地址+偏移地址
+			asm_cmd << getStackTopToD()
+			asm_cmd << "M=D"<< "\n" #pop D到相应位置
+			asm_cmd << "@SP"<< "\n"
+			asm_cmd << "M=M-1"<< "\n" #指针减1
+		end
+
+		if asm_cmd.length > 0
+			@sam_file.write(asm_cmd)
+		end	
+	end
+
+	# 第八章实现
+
+	def writeLabel(label)
+		asm_cmd = ""
+		asm_cmd << "(#{label})"<< "\n"
+		@sam_file.write(asm_cmd)
+	end
+
+	def writeGoto(label)
+		asm_cmd = ""
+		asm_cmd << "@#{label}"<< "\n"
+		asm_cmd << "0;JMP"<< "\n"
+		@sam_file.write(asm_cmd)
+	end
+
+	def writeIf(label)
+		asm_cmd = ""
+		asm_cmd << "@SP"<< "\n"
+		asm_cmd << "A=M"<< "\n"
+		asm_cmd << "A=A-1"<< "\n"
+		asm_cmd << "D=M"<< "\n"
+		asm_cmd << "@SP"<< "\n"
+		asm_cmd << "M=M-1"<< "\n" #指针减1
+		asm_cmd << "@#{label}"<< "\n"
+		asm_cmd << "D;JNE"<< "\n"
+		@sam_file.write(asm_cmd)
+	end
+
+	def writeFunction(funtionName, numArgs)
+		asm_cmd = "(#{funtionName})"<< "\n"
+		@sam_file.write(asm_cmd)
+		index = 0
+		while index < numArgs.to_i
+			writePushPop("C_PUSH", "constant", "0")
+			index = index + 1
+		end
+	end
+
+	def writeCall(functionName, numArgs)
+		writePushPop("C_PUSH", "constant", "callLabel#{@callIndex}")
+		saveSegment("LCL")
+		saveSegment("ARG")
+		saveSegment("THIS")
+		saveSegment("THAT")
+		resetARG(5+numArgs.to_i)
+		resetLCL()
+		callJump(functionName, "callLabel#{@callIndex}")
+		@callIndex = @callIndex + 1
+	end
+
+	def writeReturn
+		saveReturnAddress()
+		writePushPop("C_POP", "argument", "0") #pop return value到caller的argument段
+		restoreSP()
+		restoreSegement("THAT")
+		restoreSegement("THIS")
+		restoreSegement("ARG")
+		restoreSegement("LCL")
+		gotoReturnAddress()
+	end
+
+	def writeSysInit()
+		asm_cmd = "@256"<< "\n"
+		asm_cmd << "D=A"<< "\n"
+		asm_cmd << "@SP"<< "\n"
+		asm_cmd << "M=D"<< "\n" #sp=256
+		@sam_file.write(asm_cmd) 
+	 	writeCall("Sys.init", 0) # call Sys.init
+	end 
+
+	# 辅助函数
+
+	def saveReturnAddress() #暂存return地址到R14, return地址存在SP-5，R13在getStackTopToD过程中被修改了
+		asm_cmd = "@5"<< "\n"
+		asm_cmd << "D=A"<< "\n"
+		asm_cmd << "@LCL"<< "\n"
+		asm_cmd << "A=M-D"<< "\n"
+		asm_cmd << "D=M"<< "\n"
+		asm_cmd << "@R14"<< "\n"
+		asm_cmd << "M=D"<< "\n"
+		@sam_file.write(asm_cmd)
+	end
+
+	def restoreSP()
+		asm_cmd = "@ARG"<< "\n"
+		asm_cmd << "D=M+1"<< "\n"
+		asm_cmd << "@SP"<< "\n"
+		asm_cmd << "M=D"<< "\n"
+		@sam_file.write(asm_cmd)
+	end
+
+	def restoreSegement(segment) #恢复segement
+		table = {"THAT"=>"1", "THIS"=>"2", "ARG"=>"3", "LCL"=>"4"}
+		asm_cmd = "@#{table[segment]}"<< "\n"
+		asm_cmd << "D=A"<< "\n"
+		asm_cmd << "@LCL"<< "\n"
+		asm_cmd << "A=M-D"<< "\n"
+		asm_cmd << "D=M"<< "\n"
+		asm_cmd << "@#{segment}"<< "\n"
+		asm_cmd << "M=D"<< "\n"
+		@sam_file.write(asm_cmd)
+	end
+
+	def gotoReturnAddress() #return到上一级函数继续执行
+		asm_cmd = "@R14"<< "\n"
+		asm_cmd << "A=M"<< "\n" # 取出缓存的返回地址
+		asm_cmd << "0;JMP"<< "\n"
+		@sam_file.write(asm_cmd)
+	end
+
+	def saveSegment(segment)
+		asm_cmd = "@#{segment}"<< "\n"
+		asm_cmd << "D=M"<< "\n"
+		asm_cmd << pushDtoStack()
+		@sam_file.write(asm_cmd)
+	end
+
+	def resetARG(steps) #ARG设置为SP指针向前移动step步
+		asm_cmd = "@SP"<< "\n"
+		asm_cmd << "D=M"<< "\n"
+		asm_cmd << "@#{steps}"<< "\n"
+		asm_cmd << "D=D-A"<< "\n" 
+		asm_cmd << "@ARG"<< "\n"
+		asm_cmd << "M=D"<< "\n"
+		@sam_file.write(asm_cmd)
+	end
+
+	def resetLCL() #ARG设置为SP指针相同位置
+		asm_cmd = "@SP"<< "\n"
+		asm_cmd << "D=M"<< "\n"
+		asm_cmd << "@LCL"<< "\n"
+		asm_cmd << "M=D"<< "\n" 
+		@sam_file.write(asm_cmd)
+	end
+
+	def callJump(functionName, returnLabel) #跳转执行
+		asm_cmd = "@#{functionName}"<< "\n"
+		asm_cmd << "0;JMP"<< "\n"
+		asm_cmd << "(#{returnLabel})"<< "\n"
+		@sam_file.write(asm_cmd)
+	end
+
+	def pushConstant(const) #实现简单的push const
+		asm_cmd = "@#{const}\n"
+		asm_cmd << "D=A"<< "\n"
+		asm_cmd << pushDtoStack()
+		return asm_cmd
+	end
+
+	def pushDtoStack
+		asm_cmd = "@SP"<< "\n"
+		asm_cmd << "A=M"<< "\n"
+		asm_cmd << "M=D"<< "\n"
+		asm_cmd << "@SP"<< "\n"
+		asm_cmd << "M=M+1"<< "\n"
+		return asm_cmd
+	end
+
+	def getStackTopToD
+		asm_cmd = "D=A"<< "\n" 
+		asm_cmd << "@R13"<< "\n"
+		asm_cmd << "M=D"<< "\n" #先把A的值暂存到R13,后面在恢复
+		asm_cmd << "@SP"<< "\n"
+		asm_cmd << "A=M-1"<< "\n"
+		asm_cmd << "D=M"<< "\n" #取出值放入D
+		asm_cmd << "@R13"<< "\n"
+		asm_cmd << "A=M"<< "\n" #恢复A的值
+		return asm_cmd
+	end
+
+	def passArgsAddressToA(segment, index)
+		asm_cmd = ''
+		if  segment == "static"
+			asm_cmd << "@#{@source_file_name}.#{index}"<< "\n" #D=index
+			return asm_cmd
+		end
+
+		asm_cmd << "@#{index}"<< "\n"
+		asm_cmd << "D=A"<< "\n" #D=index
+		if  segment == "temp"
+			asm_cmd << "@R5"<< "\n"
+			asm_cmd << "A=A+D"<< "\n" #A=寄存器+偏移地址
+			puts "执行了#{segment}"
+		elsif segment == "pointer"
+			asm_cmd << "@THIS"<< "\n"
+			asm_cmd << "A=A+D"<< "\n" #A=THIS+偏移地址
+		else
+			asm_cmd << "@#{segment}"<< "\n"
+			asm_cmd << "A=M+D"<< "\n" #A=段地址+偏移地址
+		end
+		return asm_cmd
+	end
+
+	def arithmetic(cmd)
+		asm_cmd = ""
+		if cmd == 'add' #计算x+y
+			asm_cmd << passYtoD()
+			asm_cmd << "A=A-1"<< "\n"
+			asm_cmd << "D=D+M"<< "\n" #D存储x+y
+			asm_cmd << "M=D"<< "\n"
+			asm_cmd << resetSP()
+		elsif cmd == 'sub' #计算x-y
+			asm_cmd << passYtoD()
+			asm_cmd << "A=A-1"<< "\n"
+			asm_cmd << "D=M-D"<< "\n" #D存储x-y
+			asm_cmd << "M=D"<< "\n"
+			asm_cmd << resetSP()
+		elsif cmd == 'neg' #-y
+			asm_cmd << passYtoD()
+			asm_cmd << "M=-D"<< "\n" #M=-D
+			asm_cmd << resetSP()
+		elsif cmd == 'eq' #判断相等
+			asm_cmd << passYtoD()
+			asm_cmd << compare("JEQ")
+			asm_cmd << resetSP()
+		elsif cmd == 'gt' #判断大于
+			asm_cmd << passYtoD()
+			asm_cmd << compare("JGT")
+			asm_cmd << resetSP()
+		elsif cmd == 'lt' #判断小于
+			asm_cmd << passYtoD()
+			asm_cmd << compare("JLT")
+			asm_cmd << resetSP()
+		elsif cmd == 'and'
+			asm_cmd << passYtoD()
+			asm_cmd << "A=A-1"<< "\n"
+			asm_cmd << "D=D&M"<< "\n" #D存储x&y
+			asm_cmd << "M=D"<< "\n"
+			asm_cmd << resetSP()
+		elsif cmd == 'or'
+			asm_cmd << passYtoD()
+			asm_cmd << "A=A-1"<< "\n"
+			asm_cmd << "D=D|M"<< "\n" #D存储x|y
+			asm_cmd << "M=D"<< "\n"
+			asm_cmd << resetSP()
+		elsif cmd == 'not'
+			asm_cmd << passYtoD()
+			asm_cmd << "M=!D"<< "\n"
+		end
+		return asm_cmd
+	end
+
+	def passYtoD
+		asm_cmd = ""
+		asm_cmd << "@SP"<< "\n"
+		asm_cmd << "A=M"<< "\n"
+		asm_cmd << "A=A-1"<< "\n"
+		asm_cmd << "D=M"<< "\n" #D存储y
+		return asm_cmd
+	end
+
+	def resetSP
+		asm_cmd = ""
+		asm_cmd << "D=A"<< "\n"
+		asm_cmd << "@SP"<< "\n"
+		asm_cmd << "M=D+1"<< "\n" #SP指针重置
+		return asm_cmd
+	end
+
+	def compare(cmd) #xy大小判断
+		asm_cmd = ""
+		asm_cmd << "A=A-1"<< "\n"
+		asm_cmd << "D=M-D"<< "\n" #D存储x-y
+		asm_cmd << "@branchLabel_#{@branchIndex}"<< "\n"
+		asm_cmd << "D;#{cmd}"<< "\n"
+		asm_cmd << "D=0"<< "\n" #0代表false
+		asm_cmd << "@mergeLabel_#{@mergeIndex}"<< "\n"
+		asm_cmd << "0;JMP"<< "\n"
+		asm_cmd << "(branchLabel_#{@branchIndex})"<< "\n"
+		asm_cmd << "D=-1"<< "\n" #-1代表true
+		asm_cmd << "(mergeLabel_#{@mergeIndex})"<< "\n"
+		asm_cmd << "@SP"<< "\n"
+		asm_cmd << "A=M-1"<< "\n"
+		asm_cmd << "A=A-1"<< "\n"
+		asm_cmd << "M=D"<< "\n"
+		@branchIndex = @branchIndex + 1
+		@mergeIndex = @mergeIndex + 1
+		return asm_cmd
+	end
+
+	def close
+		@sam_file.close()
+	end
+end
---
+++ projects/08/FunctionCalls/StaticsTest/Parser.rb
@@ -0,0 +1,89 @@
+class Parser
+	def initialize(file)
+		@current_index = -1
+		@current_command = ""
+		@command_array = Array.new()
+		@current_symbol = ""
+		@curren_command_type = ""
+
+		file.split("\n").each do |l|
+			line = l.strip
+			if  (line.include? "//")
+				line = line.split("//")[0]
+			end
+			if line.length != 0 && !(line.include? "//")
+				@command_array << line
+			end
+		end
+
+		# puts "source file:"
+		# puts @command_array
+	end
+
+	def reset
+		@current_index = -1
+		@current_command = ""
+		@current_symbol = ""
+		@curren_command_type = ""
+	end
+
+	def hasMoreCommands
+		return @current_index+1 < @command_array.size
+	end
+
+	def advance
+		@current_index += 1
+		@current_command = @command_array[@current_index]
+		puts "current_command: #{@current_command}" 
+	end
+
+	def commandType
+		if @current_command =~ /push(.*)/
+			puts "commandType: C_PUSH"
+			@curren_command_type = "C_PUSH"
+		elsif @current_command =~ /pop(.*)/
+			puts "commandType: C_POP"
+			@curren_command_type = "C_POP"
+		elsif @current_command =~ /label(.*)/
+			puts "commandType: C_LABEL"
+			@curren_command_type = "C_LABEL"
+		elsif @current_command =~ /if-goto(.*)/
+			puts "commandType: C_IF"
+			@curren_command_type = "C_IF"
+		elsif @current_command =~ /goto(.*)/
+			puts "commandType: C_GOTO"
+			@curren_command_type = "C_GOTO"
+		elsif @current_command =~ /function(.*)/
+			puts "commandType: C_FUNCTION"
+			@curren_command_type = "C_FUNCTION"
+		elsif @current_command =~ /return(.*)/
+			puts "commandType: C_RETURN"
+			@curren_command_type = "C_RETURN"
+		elsif @current_command =~ /call(.*)/
+			puts "commandType: C_CALL"
+			@curren_command_type = "C_CALL"
+		else
+			puts "commandType: C_ARITHMETIC"
+			@curren_command_type = "C_ARITHMETIC"
+		end
+		return @curren_command_type
+	end
+
+	def arg1
+		tokenArray = @current_command.gsub(/\s+/m, ' ').strip.split(" ")
+		if tokenArray.length > 1
+			return tokenArray[1]
+		else
+			return tokenArray[0]
+		end
+	end
+
+	def arg2
+		tokenArray = @current_command.gsub(/\s+/m, ' ').strip.split(" ")
+		if tokenArray.length > 2
+			return tokenArray[2]
+		else
+			return ''
+		end
+	end
+end
