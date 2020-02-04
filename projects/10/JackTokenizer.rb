261e7bd5bcb4b9098f6342ddc8e045cde0eab5ec
guosai <guosainpu@gmail.com>
2020-02-04 22:01:17 +0800

    开始第10章

---
+++ projects/10/JackTokenizer.rb
@@ -0,0 +1,28 @@
+require_relative 'JackTokenizer'
+require_relative 'compilationEngine' 
+
+def compileTokenflow(tokenier, outputfile)
+	compilationEngine = compilationEngine.new(tokenier, outputfile)
+	compilationEngine.compile()
+end
+
+# main 
+file_name_or_dir_name = ARGV[0]
+
+if File.file?(file_name_or_dir_name)
+	# 翻译单个文件
+	xml_file = File.new(file_name_or_dir_name.split(".")[0] + ".xml", "w")
+	tokenier = JackTokenizer.new(File.read(filename))
+	compileTokenflow(tokenier, xml_file)
+elsif File.directory?(file_name_or_dir_name)
+	# 翻译多文件
+	Dir.foreach(file_name_or_dir_name) do |filename|
+  		if filename.split(".")[1] == "jack"
+			tokenier = JackTokenizer.new(File.read(filename))
+			compileTokenflow(tokenier, xml_file)
+  		end
+	end
+end 
+
+
+
