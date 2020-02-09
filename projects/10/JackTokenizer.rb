d0c85c25efa6e598e0453851afd6364d0768f1c3
guosai <guosainpu@gmail.com>
2020-02-09 21:53:00 +0800

    解析成token

--- projects/10/JackTokenizer.rb
+++ projects/10/JackTokenizer.rb
@@ -1,3 +1,5 @@
+require 'strscan'
+
 class JackTokenizer
 
 def initialize(file, fileName)
@@ -6,9 +8,49 @@ def initialize(file, fileName)
 end
 
 def analysizeFile(file)
-	file = file.gsub(/((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/, '').split.join(' ') #删除所有注释
+	#删除所有注释
+	file = file.gsub(/((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/, '').split.join(' ')
 	puts "remove comments:"
 	puts file
+
+	#代码转成token
+	self.scanTextToToken(file)
+end
+
+def scanTextToToken(text)
+	@tokens = []
+	scanner = StringScanner.new(text)
+	#puts !scanner.eos?
+	while !scanner.eos?
+		# 扫描字符串类型
+		stringToken = scanner.scan(/"[^"]*"/)
+		if stringToken
+			#puts "追加stringToken:#{stringToken}"
+			@tokens << stringToken
+			scanner.scan(/\s+/)
+			next
+		end
+		# 扫描symbol
+		symbolToken = scanner.scan(/\{|\}|\(|\)|\[|\]|\.|\,|\;|\+|\-|\*|\/|\&|\||\<|\>|\=|\~/)
+		if symbolToken
+			#puts "追加symbolToken:#{symbolToken}"
+			@tokens << symbolToken
+			scanner.scan(/\s+/)
+			next
+		end
+		# 扫描其他tokenotherToken
+		otherToken = scanner.scan_until(/\s+|\{|\}|\(|\)|\[|\]|\.|\,|\;|\+|\-|\*|\/|\&|\||\<|\>|\=|\~/)
+		if otherToken
+			otherToken = otherToken[0..otherToken.length-2]
+			#puts "追加otherToken:#{otherToken}"
+			scanner.pos = scanner.pos-1
+			@tokens << otherToken
+			scanner.scan(/\s+/)
+			next
+		end
+	end
+	puts "解析成token"
+	puts @tokens
 end
 
 # public method
